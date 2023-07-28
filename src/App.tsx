d79cb029665d165c0a4f053dd6d17662e03119d2
Max Zhang <48145854+chetbae@users.noreply.github.com>
2023-07-27 23:40:32 -0400

    ClimbingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
-import { ThemeProvider } from 'styled-components';
-import { Router, Route, Switch } from 'react-router-dom';
-import history from './history';
-import { LandingPage, AboutPage, MusicPage, ProjectsPage } from './containers';
-import { ThemeManager } from './functions/themeManager';
-import { containers, light_mode, dark_mode } from './styles';
-import { Toggle } from './components/Toggle';
+import { ThemeProvider } from "styled-components";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./history";
+import {
+    LandingPage,
+    AboutPage,
+    MusicPage,
+    ProjectsPage,
+    ClimbingPage,
+} from "./containers";
+import { ThemeManager } from "./functions/themeManager";
+import { containers, light_mode, dark_mode } from "./styles";
+import { Toggle } from "./components/Toggle";
 
 function App() {
-  const themeProp = ThemeManager();
-  const themeMode = themeProp.theme ? dark_mode : light_mode; 
+    const themeProp = ThemeManager();
+    const themeMode = themeProp.theme ? dark_mode : light_mode;
 
-  return (
-    <ThemeProvider theme={themeMode} >
-      <containers.SiteContainer>
-      <Toggle {...themeProp} />
-      <Router history={history}>
-        <Switch>
-          <Route exact path='/' component={LandingPage} />
-          <Route path='/kurzgesagt' component={AboutPage} />
-          <Route path='/muusika' component={MusicPage} />
-          <Route path='/projects' component={ProjectsPage} />
-        </Switch>
-      </Router>
-      </containers.SiteContainer>
-    </ThemeProvider>
+    return (
+        <ThemeProvider theme={themeMode}>
+            <containers.SiteContainer>
+                <Toggle {...themeProp} />
+                <Router history={history}>
+                    <Switch>
+                        <Route exact path="/" component={LandingPage} />
+                        <Route path="/kurzgesagt" component={AboutPage} />
+                        <Route path="/projects" component={ProjectsPage} />
+                        <Route path="/muusika" component={MusicPage} />
+                        <Route path="/escalade" component={ClimbingPage} />
+                    </Switch>
+                </Router>
+            </containers.SiteContainer>
+        </ThemeProvider>
     );
 }
 
 export default App;
-
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,5 @@
-export { LandingPage } from './LandingPage.tsx';
-export { AboutPage } from './AboutPage.tsx';
-export { MusicPage } from './MusicPage.tsx';
-export { ProjectsPage } from './ProjectsPage.tsx';
+export { LandingPage } from "./LandingPage.tsx";
+export { AboutPage } from "./AboutPage.tsx";
+export { MusicPage } from "./MusicPage.tsx";
+export { ProjectsPage } from "./ProjectsPage.tsx";
+export { ClimbingPage } from "./ClimbingPage.tsx";
